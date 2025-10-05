c1792a0706e54ac23371ec07d67e10023ea0aa5c
moonga43 <mediatemoonga@gmail.com>
2025-10-05 21:16:22 +0200

    Biginner level

---
+++ Exercise4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+
+       int n;
+
+        printf("Enter a number: ");
+        scanf("%d",&n);
+
+if (n>0) {
+
+printf("Positive");
+
+} else {
+
+printf("Non-positive");
+
+}
+
+return 0;
+
+}
